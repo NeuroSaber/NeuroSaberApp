16c1a58504e6a8bcc312970f6ba78fb232a32c64
Hildemir Regis <you@example.com>
2017-09-05 18:24:27 -0300

    addd img

---
+++ platforms/android/assets/www/build/1.main.js
@@ -0,0 +1,41 @@
+webpackJsonp([1],{
+
+/***/ 374:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__post1__ = __webpack_require__(277);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Post1PageModule", function() { return Post1PageModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+var Post1PageModule = (function () {
+    function Post1PageModule() {
+    }
+    return Post1PageModule;
+}());
+Post1PageModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_1__post1__["a" /* Post1Page */],
+        ],
+        imports: [],
+        exports: [
+            __WEBPACK_IMPORTED_MODULE_1__post1__["a" /* Post1Page */]
+        ]
+    })
+], Post1PageModule);
+
+//# sourceMappingURL=post1.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=1.main.js.map
